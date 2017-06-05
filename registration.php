00da5516958c66c85b63c84dab3a93a941be3cf1
rajendrak <rajendrak@kensium.com>
2017-06-05 16:30:47 +0530

    Initial commit

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @category   Amconnector
+ * @package    Kensium_Amconnector
+ * @copyright  Copyright (c) 2016 Kensium Solution Pvt.Ltd. (http://www.kensiumsolutions.com/)
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Kensium_Welcome',
+    __DIR__
+);
